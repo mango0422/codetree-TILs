bcb6148cc7b5b72fec68317de9b572d6ae52a6fc
mango0422 <83891638+mango0422@users.noreply.github.com>
2024-09-27 01:51:20 +0900

    Title: 그 계절, 그 날, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240927/그 계절, 그 날/that-season-that-day.java
+++ 240927/그 계절, 그 날/that-season-that-day.java
@@ -7,46 +7,47 @@ public class Main {
         int m = sc.nextInt();
         int d = sc.nextInt();
         
-        if(m == 12 || m == 1){
-            if( d > 0 && d < 32 ){
-                System.out.println("Winter");
-            }
-        } else if(m == 2){
-            if ( d > 0 && d < 29 ){
-                System.out.println("Winter");
-            } else if( d == 29 && (y % 4 == 0) ){
-                if(y % 100 !=0 || y % 400 == 0){
-                    System.out.println("Winter");
-                } else{
-                    System.out.println("-1");
-                }
-            }
-        } else if(m == 3 || m == 5){
-            if( d > 0 && d < 32 ){
-                System.out.println("Spring");
-            }
-        } else if(m == 4){
-            if( d > 0 && d < 31 ){
-                System.out.println("Spring");
-            }
-        } else if(m==6){
-            if( d > 0 && d < 31 ){
-                System.out.println("Summer");
-            }
-        } else if(m == 7 || m == 8){
-            if( d > 0 && d < 32 ){
-                System.out.println("Summer");
-            }
-        } else if(m == 9 || m == 11){
-            if( d > 0 && d < 31 ){
-                System.out.println("Fall");
-            }
-        } else if(m == 10){
-            if( d > 0 && d < 32 ){
-                System.out.println("Fall");
-            }
-        } else{
+        if (isValidDate(y, m, d)) {
+            System.out.println(getSeason(m));
+        } else {
             System.out.println("-1");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0 && year % 400 != 0) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidDate(int year, int month, int day) {
+        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month == 2) {
+            if (isLeapYear(year)) {
+                return day >= 1 && day <= 29;
+            } else {
+                return day >= 1 && day <= 28;
+            }
+        }
+        if (month >= 1 && month <= 12) {
+            return day >= 1 && day <= daysInMonth[month];
+        }
+        return false;
+    }
+
+    public static String getSeason(int month) {
+        if (month == 12 || month == 1 || month == 2) {
+            return "Winter";
+        } else if (month >= 3 && month <= 5) {
+            return "Spring";
+        } else if (month >= 6 && month <= 8) {
+            return "Summer";
+        } else {
+            return "Fall";
         }
     }
 }
