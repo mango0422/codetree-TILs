2c58c531972d63dad7551c150d0f4e8efae5e432
mango0422 <83891638+mango0422@users.noreply.github.com>
2024-09-27 01:52:05 +0900

    Title: 그 계절, 그 날, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240927/그 계절, 그 날/that-season-that-day.java
+++ 240927/그 계절, 그 날/that-season-that-day.java
@@ -6,8 +6,10 @@ public class Main {
         int y = sc.nextInt();
         int m = sc.nextInt();
         int d = sc.nextInt();
-        
-        if (isValidDate(y, m, d)) {
+
+        boolean isLeap = isLeapYear(y);
+
+        if (isValidDate(y, m, d, isLeap)) {
             System.out.println(getSeason(m));
         } else {
             System.out.println("-1");
@@ -15,39 +17,33 @@ public class Main {
     }
 
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0) {
-            if (year % 100 == 0 && year % 400 != 0) {
-                return false;
-            }
-            return true;
-        }
-        return false;
+        return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
     }
 
-    public static boolean isValidDate(int year, int month, int day) {
+    public static boolean isValidDate(int year, int month, int day, boolean isLeap) {
         int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if (month == 2) {
-            if (isLeapYear(year)) {
-                return day >= 1 && day <= 29;
-            } else {
-                return day >= 1 && day <= 28;
-            }
+
+        if (isLeap) {
+            daysInMonth[2] = 29;
         }
-        if (month >= 1 && month <= 12) {
-            return day >= 1 && day <= daysInMonth[month];
+
+        if (month < 1 || month > 12) {
+            return false;
         }
-        return false;
+
+        return day >= 1 && day <= daysInMonth[month];
     }
 
     public static String getSeason(int month) {
-        if (month == 12 || month == 1 || month == 2) {
-            return "Winter";
-        } else if (month >= 3 && month <= 5) {
-            return "Spring";
-        } else if (month >= 6 && month <= 8) {
-            return "Summer";
-        } else {
-            return "Fall";
+        switch (month) {
+            case 12: case 1: case 2:
+                return "Winter";
+            case 3: case 4: case 5:
+                return "Spring";
+            case 6: case 7: case 8:
+                return "Summer";
+            default:
+                return "Fall";
         }
     }
 }
